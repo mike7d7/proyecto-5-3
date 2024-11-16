cc343170f080b07fe3dc2c379f19c6e6765e3461
mike7d7 <mike7d7@proton.me>
2024-11-15 19:04:44 -0600

    add sql script to import data into db

---
+++ import.sql
@@ -0,0 +1,5 @@
+.import csv/usuarios.csv usuarios --csv
+.import csv/sucursales.csv sucursales --csv
+.import csv/productos.csv productos --csv
+.import csv/pedidos.csv pedidos --csv
+.import csv/incluye.csv incluye --csv
