0a09d8c07213eb23f8cd831df2f638768af90ce9
mike7d7 <mike7d7@proton.me>
2024-11-15 22:21:58 -0600

    update titles

--- pedidos.php
+++ pedidos.php
@@ -6,7 +6,7 @@ session_start();
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Actividad 4</title>
+    <title>Pedidos</title>
 <link rel="stylesheet" href="style.css" />
       </head>
   <body>
